32d6f3a858425f6fab4a2cf232fc691bfb552621
Chandu2020 <Chandu2020@LAPTOP-C8LF03CP.myfiosgateway.com>
2021-02-18 21:24:58 -0500

    first proj

---
+++ eclipse-workspace/Project1/src/firstpack/Website.java
@@ -0,0 +1,16 @@
+package firstpack;
+
+public class Website {
+	
+   
+	public static void main(String ara[]) {
+		
+		WelcomeJava wj = new WelcomeJava();
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ eclipse-workspace/Project1/src/firstpack/WelcomeJava.java
@@ -0,0 +1,47 @@
+package firstpack;
+
+import java.io.File;
+import java.util.Date;
+
+public class WelcomeJava {
+	
+	/**
+	 * this method is writen by  Sunitha for  com,ay automation
+	 */
+	public void mytest() {
+		
+	}
+	/**
+	 * This method greeting the  person by the name
+	 * @param name
+	 */
+	public void Welcome(String name) {
+		
+	}
+	
+	/**
+	 * printe today date
+	 */
+	public void displaydate() {
+		
+		
+	}
+	@SuppressWarnings("deprecation")
+	public static void main(String ara[]) {
+		System.out.println("Welcome to java");
+		for (int  i=1 ;i<10; i++) {
+			System.out.println(i);
+		}
+		
+		File f = new File("");
+		Date dd = new Date();
+		
+	System.out.println(dd.getDay());
+	
+	WelcomeJava wj = new WelcomeJava();
+	
+		
+	}
+
+	
+}
